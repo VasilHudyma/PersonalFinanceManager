5046809f095795e470dc1ff4c486decddff0e549
Vasyl Hudyma <vasia08071998@gmail.com>
2019-11-24 16:52:07 +0200

    Added exception interceptor, refactored

--- src/main/java/com/finmanager/security/jwt/JwtTokenFilter.java
+++ src/main/java/com/finmanager/security/jwt/JwtTokenFilter.java
@@ -19,7 +19,7 @@ public class JwtTokenFilter extends GenericFilterBean {
 
     private JwtTokenProvider jwtTokenProvider;
 
-    public JwtTokenFilter(JwtTokenProvider jwtTokenProvider) {
+    JwtTokenFilter(JwtTokenProvider jwtTokenProvider) {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
--- src/main/java/com/finmanager/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/finmanager/security/jwt/JwtTokenProvider.java
@@ -38,7 +38,7 @@ public class JwtTokenProvider {
         claims.put("roles", role);
 
         Date now = new Date();
-        Date validity = new Date(now.getTime() + Long.valueOf(validityInMilliseconds));
+        Date validity = new Date(now.getTime() + Long.parseLong(validityInMilliseconds));
 
         return Jwts.builder()
                 .setClaims(claims)
@@ -48,16 +48,16 @@ public class JwtTokenProvider {
                 .compact();
     }
 
-    public Authentication getAuthentication(String token) {
+    Authentication getAuthentication(String token) {
         UserDetails userDetails = this.userDetailsService.loadUserByUsername(getUsername(token));
         return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
-    public String getUsername(String token) {
+    private String getUsername(String token) {
         return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
     }
 
-    public String resolveToken(HttpServletRequest req) {
+    String resolveToken(HttpServletRequest req) {
         String bearerToken = req.getHeader("Authorization");
         if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
             return bearerToken.substring(7);
@@ -65,15 +65,11 @@ public class JwtTokenProvider {
         return null;
     }
 
-    public boolean validateToken(String token) {
+    boolean validateToken(String token) {
         try {
             Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
 
-            if (claims.getBody().getExpiration().before(new Date())) {
-                return false;
-            }
-
-            return true;
+            return !claims.getBody().getExpiration().before(new Date());
         } catch (JwtException | IllegalArgumentException e) {
             throw new InvalidJwtAuthenticationException("Expired or invalid JWT token");
         }
